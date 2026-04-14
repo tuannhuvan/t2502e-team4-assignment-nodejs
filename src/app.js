c2b769a4be326dc65923073ea76c1a6e2a48d889
Ooloobooloo <trichuot2000@gmail.com>
2026-04-14 20:57:24 +0700

    layout.ejs changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -12,7 +14,7 @@ app.use(cookieParser());
 app.use(expressLayouts);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // ─── View Engine ──────────────────────────────────────────────────────────────
 app.set("layout", "layout");
