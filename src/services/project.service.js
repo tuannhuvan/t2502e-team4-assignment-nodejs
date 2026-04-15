205ed0a6b406b0f4bbb99bbc10bf93539b7b78e3
tuannhuvan <technology6789@gmail.com>
2026-04-15 10:30:20 +0700

    Improve the logic to automatically update the taskOrderIds array when a new task is created.

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -4,12 +4,26 @@ exports.createProject = async (data, userId) => {
   return await Project.create({
     name: data.name,
     description: data.description,
-    owner: userId
+    owner: userId,
+    taskOrdersIds: [],
   });
 };
 
 exports.getProjects = async () => {
-  return await Project.find();
+  return await Project.find().populate("owner", "fullName email");
+};
+
+exports.getProjectDetails = async (projectId) => {
+  // Lấy thông tin chi tiết của dự án, bao gồm cả thông tin thành viên
+  return await Project.findById(projectId)
+    .populate({
+      path: "members.user",
+      select: "fullName avatar email"
+    }).lean();
+};
+ // Thêm logic cập nhật thứ tự Task (phục vụ drag & drop)
+exports.moveTaskOrder = async (projectId, newTaskOrderIds) => {
+  return await Project.findByIdAndUpdate(projectId, { taskOrdersIds: newTaskOrderIds }, { new: true });
 };
 
 exports.updateProject = async (id, data, userId) => {
