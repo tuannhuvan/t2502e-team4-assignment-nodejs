c80b6b9cd9546591223cfae0cfefc2bc540ec832
tuannhuvan <technology6789@gmail.com>
2026-04-15 14:25:52 +0700

    user IDs from tokens and send notifications when project changes.

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -3,18 +3,19 @@ const notificationSocket = require("../sockets/index");
 
 exports.create = async (req, res) => {
   try {
-    const data = await service.createProject(req.body, req.user._id);
+    const userId = req.user._id; // Lấy thông tin người tạo từ token
+    const data = await service.createProject(req.body, userId);
 
     // Emit universal notification to the project room
     await notificationSocket.emitNotification({
       projectId: data._id,
-      userId: req.user._id,
+      userId,
       action: "created",
       entityType: "project",
       entityData: data
     });
 
-    res.json(data);
+    res.status(201).json(data);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -25,18 +26,18 @@ exports.getAll = async (req, res) => {
     const data = await service.getProjects();
     res.json(data);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
 exports.update = async (req, res) => {
   try {
-    const data = await service.updateProject(req.params.id, req.body, req.user._id);
+    const data = await service.updateProject(req.params.id, req.body, req.userId);
 
     // Emit universal notification to the project room
     await notificationSocket.emitNotification({
       projectId: req.params.id,
-      userId: req.user._id,
+      userId: req.userId,
       action: "updated",
       entityType: "project",
       entityData: data
@@ -50,13 +51,13 @@ exports.update = async (req, res) => {
 
 exports.remove = async (req, res) => {
   try {
-    await service.deleteProject(req.params.id, req.user._id);
+    await service.deleteProject(req.params.id, req.userId);
 
     // For deletion, we need to emit before actually deleting
     // Since the project is deleted, we'll emit with the projectId from params
     await notificationSocket.emitNotification({
       projectId: req.params.id,
-      userId: req.user._id,
+      userId: req.userId,
       action: "deleted",
       entityType: "project",
       entityData: { _id: req.params.id, name: "Deleted Project" }
